915a481318f34673219cdd2d8bf46b138c560a91
ilyas <uncognic@proton.me>
2025-09-25 18:16:24 -0400

    creation exemple avec 3 moteur et un servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Exemple.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Exemple.java
@@ -16,6 +16,7 @@ public class Exemple extends LinearOpMode {
 
     private DcMotor intake;
 
+
     @Override
     public void runOpMode() {
         shooter1 = hardwareMap.get(DcMotor.class, "shooter1");
