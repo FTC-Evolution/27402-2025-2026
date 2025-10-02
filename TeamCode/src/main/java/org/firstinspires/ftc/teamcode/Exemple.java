79231a3e8e9935fa1ff841c451ba9ee76a9c606a
ilyas <uncognic@proton.me>
2025-10-02 18:32:28 -0400

    Ajout de systeme de gradation experimentale pour la force des moteurs Nous avons cree une variable forceshooter pour pouvoir controler la force des moteurs shooter rapidement. Le bouton Y augmente la force et le bouton A baisse la force.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Exemple.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Exemple.java
@@ -13,38 +13,48 @@ public class Exemple extends LinearOpMode {
     private DcMotor shooter2;
 
     private Servo pince;
-
-
     private DcMotor intake;
-
-
+    private Double forceshooter=0.6;
 
     @Override
     public void runOpMode() {
+
         shooter1 = hardwareMap.get(DcMotor.class, "shooter1");
         shooter2 = hardwareMap.get(DcMotor.class, "shooter2");
         // intake = hardwareMap.get(DcMotor.class, "intake");
         // pince = hardwareMap.get(Servo.class, "pince");
         waitForStart();
         // run until the end of the match (driver presses STOP)
-        while (opModeIsActive()) {
+             while (opModeIsActive()) {
 
             if (gamepad1.left_trigger > 0.3) {
-                shooter1.setPower(1);
-                shooter2.setPower(-1);
+                shooter1.setPower(forceshooter);
+                shooter2.setPower(-forceshooter);
             } else if (gamepad1.right_trigger > 0.3) {
-                shooter1.setPower(-1);
-                shooter2.setPower(1);
+                shooter1.setPower(-forceshooter);
+                shooter2.setPower(forceshooter);
             } else {
                 shooter1.setPower(0);
                 shooter2.setPower(0);
             }
 
-            // intake.setPower(gamepad1.right_trigger);
+            if (gamepad1.y) {
+                if (forceshooter == 1) {
+                    return;
+                }
+                forceshooter += 0.1;
+            }
+
+            if (gamepad1.a) {
+                if (forceshooter == 0) {
+                    return;
+                }
+                forceshooter -= 0.1;
+            }
+
+       // intake.setPower(gamepad1.right_trigger);
             // intake.setPower(-gamepad1.left_trigger);
             // pince.setPosition(gamepad1.left_stick_x);
         }
     }
         }
-
-
