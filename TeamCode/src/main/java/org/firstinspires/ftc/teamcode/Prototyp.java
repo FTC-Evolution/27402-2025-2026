a8bbdb9a725264037f51cba9e558d6db470f5cdd
ilyas <uncognic@proton.me>
2025-10-07 17:22:22 -0400

    Changer le systeme de push

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Prototyp.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name="exemple")
+
+public class Prototyp extends LinearOpMode {
+    private DcMotor shooter1;
+    private DcMotor shooter2;
+
+    @Override
+    public void runOpMode() {
+
+        shooter1 = hardwareMap.get(DcMotor.class, "shooter1");
+        shooter2 = hardwareMap.get(DcMotor.class, "shooter2");
+        // intake = hardwareMap.get(DcMotor.class, "intake");
+        // pince = hardwareMap.get(Servo.class, "pince");
+        waitForStart();
+        // run until the end of the match (driver presses STOP)
+             while (opModeIsActive()) {
+
+                shooter1.setPower(gamepad1.right_stick_y / 2);
+                shooter2.setPower(-gamepad1.right_stick_y / 2);
+
+                 // intake.setPower(-gamepad1.left_trigger);
+                 // pince.setPosition(gamepad1.left_stick_x);
+                 // intake.setPower(gamepad1.right_trigger);
+           }
+    }
+        }
