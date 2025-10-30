c3385ff78f4068d14014ebef3db3836024fc6a08
Richel N <rnssaketo@proton.me>
2025-10-30 18:23:35 -0400

    Add new code for the goober and learn how to invoke the IMU and useful telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Prototyp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Prototyp.java
@@ -5,10 +5,11 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareDevice;
 import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.ImuOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.Servo;
-
-
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 
 import org.firstinspires.ftc.robotcontroller.external.samples.RobotTeleopMecanumFieldRelativeDrive;
@@ -19,27 +20,62 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 public class Prototyp extends LinearOpMode {
     private DcMotor shooter1;
     private DcMotor shooter2;
-
+    private DcMotor goober;
     private Servo shooteraim;
+
+    private ElapsedTime runtime = new ElapsedTime();
 
     private Integer divider = 1;
 
     double counter1 = 0;
     double counter2 = 0;
-
+    boolean end = false;
     String winner = "none";
 
-    private RobotTeleopMecanumFieldRelativeDrive drive;
     // This declares the four motors needed
     DcMotor frontLeftDrive;
     DcMotor frontRightDrive;
     DcMotor backLeftDrive;
     DcMotor backRightDrive;
 
+    RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.UP;
+    RevHubOrientationOnRobot.UsbFacingDirection  usbDirection  = RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
+
+    RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logoDirection, usbDirection);
+
+
     // This declares the IMU needed to get the current direction the robot is facing
     IMU imu;
+    AngleUnit angle = AngleUnit.DEGREES;
+    IMU.Parameters imup = new IMU.Parameters(orientationOnRobot);
 
-    boolean end = false;
+
+
+
+    @Override
+    public void runOpMode() {
+
+        // InitDrive();
+        // shooterInit();
+        // aimInit();
+        // buttoninnit();
+        gooberInit();
+        telemetryInit();
+        waitForStart();
+        runtime.reset();
+
+        if (isStopRequested()) return;
+
+        while(opModeIsActive()){
+            // buttonLoop();
+            //     boucleDrive();
+            //   aimLoop();
+            // shooterLoop();
+            gooberLoop();
+            telemetryLoop();
+        }
+
+    }
 
     public void shooterInit() {
         shooter1 = hardwareMap.get(DcMotor.class, "leftshooter");
@@ -53,23 +89,39 @@ public class Prototyp extends LinearOpMode {
     public void aimLoop() {
         shooteraim.setPosition(gamepad2.left_stick_y);
     }
+
+    public void gooberInit(){
+        goober = hardwareMap.get(DcMotor.class, "goober");
+
+    }
+
+    public void gooberLoop(){
+        if (gamepad2.rightBumperWasPressed()) {
+            goober.setPower(-1);
+        } else if (gamepad2.leftBumperWasPressed()) {
+            goober.setPower(1);
+        } else {
+            goober.setPower(0);
+        }
+
+    }
     public void shooterLoop() {
 
-            shooter1.setPower(gamepad2.right_stick_y / divider);
-            shooter2.setPower(-gamepad2.right_stick_y / divider);
+        shooter1.setPower(gamepad2.right_stick_y / divider);
+        shooter2.setPower(-gamepad2.right_stick_y / divider);
 
-            if (gamepad2.x) {
-                divider = 1;
-            }
-            if (gamepad2.y) {
-                divider = 2;
-            }
-            if (gamepad2.b) {
-                divider = 3;
-            }
-            if (gamepad2.a) {
-                divider = 4;
-            }
+        if (gamepad2.x) {
+            divider = 1;
+        }
+        if (gamepad2.y) {
+            divider = 2;
+        }
+        if (gamepad2.b) {
+            divider = 3;
+        }
+        if (gamepad2.a) {
+            divider = 4;
+        }
     }
 
 
@@ -112,20 +164,20 @@ public class Prototyp extends LinearOpMode {
         frontRightDrive.setPower(frontRightPower);
         backRightDrive.setPower(backRightPower);
     }
-public void buttoninnit() {
+    public void buttoninnit() {
 
 
-}
+    }
 
-public void buttonLoop() {
+    public void buttonLoop() {
 
 
         // telemetry.addLine("get 50 to win");
 
-    if (end)
-    {
-        return;
-    }
+        if (end)
+        {
+            return;
+        }
         if (gamepad1.xWasPressed()) {
             counter1 +=1;
         }
@@ -151,25 +203,30 @@ public void buttonLoop() {
         telemetry.addData("game over", end);
 
         telemetry.update();
-}
+    }
+    public void telemetryInit(){
+        telemetry.addData("status", "initialized");
+        telemetry.update();
 
-    public void runOpMode() {
+        imu = hardwareMap.get(IMU.class, "imu");
+        imu.initialize(imup);
+        imu.resetYaw();
+    }
 
-        // InitDrive();
-        // shooterInit();
-        // aimInit();
-        buttoninnit();
-        waitForStart();
+    public void telemetryLoop(){
 
-        if (isStopRequested()) return;
+        telemetry.addData("status", "runtime: " + runtime.toString());
 
-        while(opModeIsActive()){
-            buttonLoop();
-            //     boucleDrive();
-            //   aimLoop();
-            // shooterLoop();
-        }
+        telemetry.addData("yaw", imu.getRobotYawPitchRollAngles().getYaw(angle));
+        telemetry.addData("pitch", imu.getRobotYawPitchRollAngles().getPitch(angle));
+        telemetry.addData("roll", imu.getRobotYawPitchRollAngles().getRoll(angle));
 
+        telemetry.addData("angular velocity x", imu.getRobotAngularVelocity(angle).xRotationRate);
+        telemetry.addData("angular velocity y", imu.getRobotAngularVelocity(angle).yRotationRate);
+        telemetry.addData("angular velocity z", imu.getRobotAngularVelocity(angle).zRotationRate);
+
+
+        telemetry.update();
     }
 
 }
