19290e6793a0f2b0ccebfb9e90b7142d7662b0fb
Richel N <rnssaketo@proton.me>
2026-03-31 16:43:30 -0400

    I forgot to add he new IMU class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/interfaces/Brain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/interfaces/Brain.java
@@ -1,4 +1,22 @@
 package org.firstinspires.ftc.teamcode.interfaces;
 
-public class Brain {
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.IMU;
+
+public class Brain extends LinearOpMode {
+    public static IMU imu;
+    public void Init(){
+        RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.BACKWARD;
+        RevHubOrientationOnRobot.UsbFacingDirection  usbDirection  = RevHubOrientationOnRobot.UsbFacingDirection.DOWN;
+        RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logoDirection, usbDirection);
+
+        IMU.Parameters params = new IMU.Parameters(orientationOnRobot);
+
+        imu = hardwareMap.get(IMU.class, "imu");
+        imu.initialize(params);
+        imu.resetYaw();
+    }
+
+    public void runOpMode() {}
 }
