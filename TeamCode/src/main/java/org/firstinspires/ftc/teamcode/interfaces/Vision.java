35229727bf6c081f33528d63a4b99067398b8b2f
Richel N <rnssaketo@proton.me>
2026-04-09 16:26:21 -0400

    Add vision class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/interfaces/Vision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/interfaces/Vision.java
@@ -1,7 +1,120 @@
 package org.firstinspires.ftc.teamcode.interfaces;
 
+import android.util.Size;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Vision {
+    public static final int        CAMERA_RES_WIDTH       = 640;
+    public static final int        CAMERA_RES_HEIGHT      = 480;
+
+    public static final double        CAMERA_X               = -3.5;
+    public static final double        CAMERA_Y               = 0;
+    public static final double        CAMERA_Z               = 0;
+
+    public static final double        CAMERA_YAW             = 0;
+    public static final double        CAMERA_PITCH           = -80;
+    public static final double        CAMERA_ROLL            = 10;
+
+    public static final double     APRILTAG_TOLERANCE_ANGLE= 5;
+    public static final double     APRILTAG_TOLERANCE_SIDE = 8;
+
+    public String obeliskName;
+    public String fieldGoalName;
+
+    public AprilTagDetection lastSeenGoal = null;
+
+    Map<Integer, String> obeliskPositions = new HashMap<>();
+    Map<Integer, String> fieldSidePositions = new HashMap<>();
+
+
+
+    AprilTagProcessor aprilTagProcessor;
+    VisionPortal visionPortal;
+    public Vision(WebcamName camera) {
+
+        Position cameraPosition = new Position(DistanceUnit.INCH,
+                CAMERA_X,
+                CAMERA_Y,
+                CAMERA_Z, 0);
+        YawPitchRollAngles cameraOrientation = new YawPitchRollAngles(AngleUnit.DEGREES,
+                CAMERA_YAW,
+                CAMERA_PITCH,
+                CAMERA_ROLL, 0);
+
+        aprilTagProcessor = new AprilTagProcessor.Builder()
+                .setCameraPose(cameraPosition, cameraOrientation)
+                .build();
+
+        VisionPortal.Builder visionPortalBuilder = new VisionPortal.Builder();
+
+        visionPortalBuilder.setCamera(camera);
+        visionPortalBuilder.setCameraResolution(new Size(CAMERA_RES_WIDTH, CAMERA_RES_HEIGHT));
+        visionPortalBuilder.enableLiveView(true);
+
+        visionPortalBuilder.addProcessor(aprilTagProcessor);
+
+        visionPortal = visionPortalBuilder.build();
+
+        // init hashmaps
+
+        obeliskPositions.put(21,"GPP");
+        obeliskPositions.put(22,"PGP");
+        obeliskPositions.put(23,"PPG");
+
+        fieldSidePositions.put(20,"blue");
+        fieldSidePositions.put(24,"red");
+    }
+
+    public List<AprilTagDetection> getDetections() {
+        return aprilTagProcessor.getDetections();
+    }
+
+    public void updateAprilTags() {
+        for (AprilTagDetection detection : aprilTagProcessor.getDetections()) {
+            if (detection.metadata != null) {
+                if (detection.metadata.name.contains("Obelisk")) {
+                    obeliskName = obeliskPositions.get(detection.id);
+                } else {
+                    fieldGoalName = fieldSidePositions.get(detection.id);
+                    lastSeenGoal = detection;
+                }
+            }
+        }
+    }
+
+    public double getFieldGoalDistance() {
+        if (lastSeenGoal != null) {
+            return lastSeenGoal.ftcPose.range;
+        } else { return -1.0; }
+    }
+
+    public double getFieldGoalSideDistance() {
+        if (lastSeenGoal != null) {
+            return lastSeenGoal.ftcPose.x;
+        } else { return -1.0; }
+    }
+
+    public double getFieldGoalBearing() {
+        if (lastSeenGoal != null) {
+            return lastSeenGoal.ftcPose.bearing;
+        } else { return -1.0; }
+    }
+
+    public double getFieldGoalYaw() {
+        if (lastSeenGoal != null) {
+            return lastSeenGoal.ftcPose.yaw;
+        } else { return -1.0; }
+    }
 }
