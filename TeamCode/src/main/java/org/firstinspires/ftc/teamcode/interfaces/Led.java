0f827006a9ff3f3b7293e165f924c60221cd7e27
Richel N <rnssaketo@proton.me>
2026-04-21 19:26:17 -0400

    Add Colour class with Static values for colors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/interfaces/Led.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/interfaces/Led.java
@@ -4,6 +4,11 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class Led {
     private final Servo led;
+
+    public class Colour {
+        public static final double WHITE = 1;
+        public static final double GREEN = 0.5;
+    }
     public Led(Servo led){
         this.led = led;
     }
