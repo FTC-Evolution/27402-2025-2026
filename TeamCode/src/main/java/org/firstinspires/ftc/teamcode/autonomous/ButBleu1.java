1f8d3d4879e92c6ecba47af07c381d1f82cf64d4
Richel N <rnssaketo@proton.me>
2026-04-21 19:24:31 -0400

    Update autonomous modes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/ButBleu1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/ButBleu1.java
@@ -3,13 +3,31 @@ package org.firstinspires.ftc.teamcode.autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.BaseAutonomous;
+import org.firstinspires.ftc.teamcode.interfaces.Led;
+import org.firstinspires.ftc.teamcode.interfaces.Shooter;
 
 @Autonomous(name="But Bleu - Richel 1")
 public class ButBleu1 extends BaseAutonomous {
     @Override
     public void Run() {
-        drive(DRIVE_SPEED,  65.80,  65.8, 3.0);
-        drive(TURN_SPEED,   24, -24, 4.0);
-        drive(DRIVE_SPEED, -24, -24, 4.0);
+        drive(DRIVE_SPEED,  -45,  -45, 3.0); // adjust from 51
+        // drive(TURN_SPEED,   , -24, 4.0);
+
+        vision.updateGoalAprilTag();
+
+        double target_distance = 45;
+
+        // alignAprilTag(target_distance);
+
+        shooterTPS = shooter.speed(shooterPower);
+
+        shooter.fireUp(shooterTPS, led);
+
+        sleep(500);
+        goober.modPower(-1);
+        sleep(1500);
+        goober.modPower(0);
+        sleep(500);
+        shooter.modVelocity(0);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/PetitTriangleBleu.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/PetitTriangleBleu.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.BaseAutonomous;
+import org.firstinspires.ftc.teamcode.interfaces.Shooter;
 import org.firstinspires.ftc.teamcode.interfaces.Vision;
 
 import java.util.Date;
@@ -11,25 +12,14 @@ public class PetitTriangleBleu extends BaseAutonomous {
     @Override
     public void Run() {
         driveCrabe(DRIVE_SPEED, 72, -72, 5.0);
-        drive(TURN_SPEED, degreesToInches(45), -degreesToInches(45), 5.0);
+        // drive(TURN_SPEED, degreesToInches(45), -degreesToInches(45), 5.0);
 
         vision.updateGoalAprilTag();
 
-        double target_distance = 60;
+        double target_distance = 52;
 
         double goal_distance;
 
-        while (vision.absoluteFieldGoalDistance() > Vision.APRILTAG_TOLERANCE_DISTANCE + target_distance) {
-            goal_distance = vision.absoluteFieldGoalDistance() - target_distance;
-            drive(DRIVE_SPEED, goal_distance, goal_distance, 100.0);
-        }
-
-        shooter.modVelocity(shooter.speed(40));
-        sleep(500);
-        goober.modPower(1);
-        sleep(850);
-        goober.modPower(0);
-        sleep(500);
-        shooter.modVelocity(0);
+        // alignAprilTag(target_distance);
     }
 }
