fee9c18c46bdcbaf6ed797f88829f21b475e8f05
Richel N <rnssaketo@proton.me>
2026-04-28 19:10:22 -0400

    Us enew format in PetitiTrianglebleu

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/PetitTriangleBleu.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/PetitTriangleBleu.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.BaseAutonomous;
+import org.firstinspires.ftc.teamcode.interfaces.Vision;
 
 @Autonomous(name="Petit Triangle Bleu")
 public class PetitTriangleBleu extends BaseAutonomous {
@@ -10,7 +11,7 @@ public class PetitTriangleBleu extends BaseAutonomous {
         driveCrabe(DRIVE_SPEED, 72, -72, 5.0);
         // drive(TURN_SPEED, degreesToInches(45), -degreesToInches(45), 5.0);
 
-        vision.updateGoalAprilTag();
+        vision.updateGoalAprilTag(Vision.UpdateGoalAprilTagGoal.BLUE);
 
         double target_distance = 52;
 
