a5838b852972d3703b02a85aa7b29137c266a9bd
Richel N <rnssaketo@proton.me>
2026-05-12 19:19:59 -0400

    Higher deadband for robot tunr

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseOpMode.java
@@ -119,7 +119,7 @@ public class BaseOpMode extends LinearOpMode {
                 0.002,
                 -0.3,
                 0.3,
-                0.1
+                1
         );
     }
     class full {
