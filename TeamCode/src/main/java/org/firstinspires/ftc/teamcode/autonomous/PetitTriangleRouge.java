a7a86a4a2126330939297fd43cf2b71ecce6038f
Richel N <rnssaketo@proton.me>
2026-05-14 17:25:27 -0400

    Add twin autonomous mode of PetitTriangleBleu.java for red goal

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/PetitTriangleRouge.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.BaseAutonomous;
+import org.firstinspires.ftc.teamcode.interfaces.Shooter;
+
+@Autonomous(name="Petit Triangle Rouge")
+public class PetitTriangleRouge extends BaseAutonomous {
+    @Override
+    public void Run() {
+        brain.resetYaw();
+        // alignFieldGoal(2500, 115, Vision.UpdateGoalAprilTagGoal.BLUE, 34.5);
+
+        shooterTPS = shooter.speed(Shooter.DEFAULT_LONG_AUTONOMOUS_SHOOTER_POWER);
+
+        shooter.fireUp(shooterTPS, led);
+
+        sleep(3000);
+        goober.modPower(-1);
+        sleep(2500);
+        goober.modPower(0);
+        sleep(500);
+        shooter.modVelocity(0);
+
+        rotate(2500, -70);
+
+        sleep(750);
+
+        drive(DRIVE_SPEED, 10, 10, 5.0);
+
+        // requestOpModeStop();
+    }
+}
