8dc659cf8e5306cda935da6f179e5e8f7953404e
Richel N <rnssaketo@proton.me>
2026-05-14 17:22:12 -0400

    Add ButRouge1.java - red goal version of automode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/ButRouge1.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.BaseAutonomous;
+import org.firstinspires.ftc.teamcode.interfaces.Shooter;
+import org.firstinspires.ftc.teamcode.interfaces.Vision;
+
+@Autonomous(name="But Rouge - Richel 1")
+public class ButRouge1 extends BaseAutonomous {
+    @Override
+    public void Run() {
+        brain.resetYaw();
+        drive(DRIVE_SPEED,  -45,  -45, 3.0); // adjust from 51
+        // drive(TURN_SPEED,   , -24, 4.0);
+
+        alignFieldGoal(2500, 45, Vision.UpdateGoalAprilTagGoal.RED,0);
+
+        shooterTPS = shooter.speed(Shooter.DEFAULT_SHOOTER_POWER);
+
+        shooter.fireUp(shooterTPS, led);
+
+        sleep(1000);
+        goober.modPower(-1);
+        sleep(2500);
+        goober.modPower(0);
+        sleep(500);
+        shooter.modVelocity(0);
+
+        rotate(2500, -35);
+
+        sleep(750);
+
+        drive(DRIVE_SPEED, 45, 45, 5.0);
+
+        // requestOpModeStop();
+    }
+}
