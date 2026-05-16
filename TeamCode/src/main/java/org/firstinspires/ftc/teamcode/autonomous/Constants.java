4af6bd29953894eb9dd201e9cf720168b98e3d41
Effectivement <richel265@tuta.io>
2026-05-16 15:37:21 -0400

    add constants for automodes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Constants.java
@@ -0,0 +1,8 @@
+public class Constants {
+
+    public static final double ALIGN_TIMEOUT_MS = 2500;
+    public static final double ALIGN_DISTANCE_IN = 45;
+    public static final double DRIVE_TIMEOUT_S = 3.0;
+    public static final double INTAKE_LONG_MS = 2500;
+    public static final double ROTATE_TIMEOUT_MS = 2500;
+}
