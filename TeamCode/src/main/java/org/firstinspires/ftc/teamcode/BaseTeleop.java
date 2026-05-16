4abd2e20a64d607835cf2449405802ae2792195c
Effectivement <richel265@tuta.io>
2026-05-16 11:28:05 -0400

    clean up imports in BaseTeleop.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseTeleop.java
@@ -1,9 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-
-
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.interfaces.Vision;
 
